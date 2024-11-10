ea9261cf1e427e22a3da478a43da2b6e9c81bc2d
Vallabh Jagtap <95693051+Vjgatap@users.noreply.github.com>
2024-11-10 18:16:29 +0000

    while loop is done

---
+++ Basic/while.cpp
@@ -0,0 +1,21 @@
+// while loop in cpp
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int number;
+    cout<<"Enter a number : ";
+    cin>>number;
+
+    int i = 0;
+    while(i<=number){
+        cout<<"Jay Shree Ram"<<endl;
+       i++;
+    }
+     
+
+    return 0;
+}
