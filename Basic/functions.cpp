bf5358c651f193b25f6031c2ae3a6bb799aec8e0
Vallabh Jagtap <95693051+Vjgatap@users.noreply.github.com>
2024-11-10 18:17:55 +0000

    switch case is done

---
+++ Basic/functions.cpp
@@ -0,0 +1,18 @@
+// function in cpp
+
+#include<iostream>
+using namespace std;
+
+void myfun(){
+
+    for(int i = 0; i <= 9; i++){
+        cout<<"Har Har Mahadev !"<<endl;
+    }
+}
+
+int main(){
+
+    myfun();
+    return 0;
+
+}
