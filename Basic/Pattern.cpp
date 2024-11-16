b47eff77481c675d7eab7cbc6ec05dbdb7c3520e
Vallabh Jagtap <95693051+Vjgatap@users.noreply.github.com>
2024-11-16 13:39:34 +0000

    Pattern Program done!

---
+++ Basic/Pattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(auto i = 0; i < 5; i++){
+        for(auto j = 0; j < 5; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
