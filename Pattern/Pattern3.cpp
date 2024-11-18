4eefb38f1ef0858ce055f9b2a6e076b8f28b6e82
Vallabh Jagtap <95693051+Vjgatap@users.noreply.github.com>
2024-11-18 18:36:52 +0000

    Pattern3 is done!

---
+++ Pattern/Pattern3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int a = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << a;
+            int b = a + 64;
+            char c = (char)b;
+            a++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
